b8e7bbb3a8452d97157d9b0c3f01d391956eff35
Simon <bardrabbit@gmail.com>
2021-01-23 15:35:37 -0500

    complete chat header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Chat from './Chat';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <div className='app_body'>
-        <Sidebar />
-      </div>
+      <Router>
+        <Header />
+        <div className='app__body'>
+          <Sidebar />
+          <Switch>
+            <Route path='/room/:roomId'>
+              <Chat />
+            </Route>
+            <Route path='/'>
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
